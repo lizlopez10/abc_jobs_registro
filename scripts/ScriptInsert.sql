7e70c9060036e9416766ce84350f9095c59ace97
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-10-19 15:07:30 -0500

    addunit test

--- scripts/ScriptInsert.sql
+++ scripts/ScriptInsert.sql
@@ -3,7 +3,7 @@ INSERT INTO registro.tipo_usuario (id,nombre) VALUES
 	 (2, 'Candidato'),
 	 (3, 'Empresa');
 	
-INSERT INTO registro.usuarios (nombre_completo,email, contrasena, role_id) VALUES
+INSERT INTO registro.usuarios (nombre_completo,email, contrasena, id_tipo_usuario) VALUES
 	 ('Zaray Rey', 'zrey@email.com', 'abc1223', 1),
 	 ('Lizeth Lopez', 'llopez@email.com', 'abc1223', 1),
 	 ('Erik Bernal', 'ebernal@email.com', 'abc1223', 1),
--- test/registro.data.test.js
+++ test/registro.data.test.js
@@ -14,50 +14,50 @@ describe('RegistroData', () => {
   it('test', async () => {
     expect(1).toBe(1);
   })
-  // it('debería seleccionar registros de la base de datos', async () => {
-  //   const tabla = 'registro.usuarios';
-  //   const columnasWhere = ['email'];
-  //   const valoresWhere = ['liz@e.com',];
+  it('debería seleccionar registros de la base de datos', async () => {
+    const tabla = 'registro.usuarios';
+    const columnasWhere = ['email'];
+    const valoresWhere = ['liz@e.com',];
 
-  //   // Configura el mock de la base de datos
-  //   db.query.mockImplementation((query, valores, callback) => {
-  //     expect(query).toBe('SELECT * FROM usuarios WHERE email = $1');
-  //     expect(valores).toEqual(['liz@e.com']);
-  //     callback(null, {rowCount: 1});
-  //   });
+    // Configura el mock de la base de datos
+    db.query.mockImplementation((query, valores, callback) => {
+      expect(query).toBe('SELECT * FROM usuarios WHERE email = $1');
+      expect(valores).toEqual(['liz@e.com']);
+      callback(null, {rowCount: 1});
+    });
 
-  //   const result = await registro.select(tabla, columnasWhere, valoresWhere);
-  //   expect(result.rowCount).toBe(1);
-  // });
+    const result = await registro.select(tabla, columnasWhere, valoresWhere);
+    expect(result.rowCount).toBe(1);
+  });
 
-  // it('debería insertar registros en la base de datos', async () => {
-  //   const tabla = 'registro.usuarios';
-  //   const columnas = ["nombre_completo", "email", "contrasena", "id_tipo_usuario"];
-  //   const valores = ["usuario", "email@usuario.com", "contrasena", 1];
+  it('debería insertar registros en la base de datos', async () => {
+    const tabla = 'registro.usuarios';
+    const columnas = ["nombre_completo", "email", "contrasena", "id_tipo_usuario"];
+    const valores = ["usuario", "email@usuario.com", "contrasena", 1];
 
-  //   // Configura el mock de la base de datos
-  //   db.query.mockImplementation((query, valores, callback) => {
-  //     expect(query).toBe('INSERT INTO usuarios (nombre_completo,email,contrasena,id_tipo_usuario) VALUES ($1,$2, $3, $4) RETURNING *');
-  //     expect(valores).toEqual(['usuario', 'email@usuario.com','contrasena', 1]);
-  //     callback(null, {rowCount: 1});
-  //   });
+    // Configura el mock de la base de datos
+    db.query.mockImplementation((query, valores, callback) => {
+      expect(query).toBe('INSERT INTO usuarios (nombre_completo,email,contrasena,id_tipo_usuario) VALUES ($1,$2, $3, $4) RETURNING *');
+      expect(valores).toEqual(['usuario', 'email@usuario.com','contrasena', 1]);
+      callback(null, {rowCount: 1});
+    });
 
-  //   const result = await registro.insert(tabla, columnas, valores);
-  //   expect(result.rowCount).toBe(1);
-  // });
-//   it('error: no debería insertar registros en la base de datos', async () => {
-//     const tabla = 'registro.usuarioss';
-//     const columnas = ["nombre_completo", "email", "contrasena", "id_tipo_usuario"];
-//     const valores = ["usuario","email", "contrasena", 1];
+    const result = await registro.insert(tabla, columnas, valores);
+    expect(result.rowCount).toBe(1);
+  });
+  it('error: no debería insertar registros en la base de datos', async () => {
+    const tabla = 'registro.usuarios';
+    const columnas = ["nombre_completo", "email", "contrasena", "id_tipo_usuario"];
+    const valores = ["usuario","email", "contrasena", 1];
 
-//     // Configura el mock de la base de datos
-//     db.query.mockImplementation((query, valores, callback) => {
-//       expect(query).toBe('INSERT INTO usuarios (nombre_completo,email,contrasena,id_tipo_usuario) VALUES ($1,$2, $3, $4) RETURNING *');
-//       expect(valores).toEqual(['usuario', 'email@usuario.com','contrasena', 1]);
-//       callback(null, "error");
-//     });
+    // Configura el mock de la base de datos
+    db.query.mockImplementation((query, valores, callback) => {
+      expect(query).toBe('INSERT INTO usuarios (nombre_completo,email,contrasena,id_tipo_usuario) VALUES ($1,$2, $3, $4) RETURNING *');
+      expect(valores).toEqual(['usuario', 'email@usuario.com','contrasena', 1]);
+      callback(null, "error");
+    });
 
-//     const result = await registro.insert(tabla, columnas, valores);
-//     expect(result.rowCount).toBe(1);
-//   });
+    const result = await registro.insert(tabla, columnas, valores);
+    expect(result.rowCount).toBe(1);
+  });
 });
