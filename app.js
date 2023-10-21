cd09ffeccc99eb9d5840f1c7fd786dcb0bdb4c24
Lizeth Lopez <lizlopez.10@hotmail.com>
2023-10-21 12:40:26 -0500

    add tls ct

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cosr = require('cors');
+var cors = require('cors');
 var registroRouter = require('./routes/registro');
 var healthcheckRouter = require('./routes/healthcheck');
 
